5489ab95129a2d9f64fbbf723c5ba769a1aa2fdc
mriganko <duttamriganko40@gmail.com>
2024-01-13 14:50:35 +0530

    Initial commit

---
+++ starter/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//routes
+
+app.use(express.static('./public'))
+app.use(express.json());
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000
+
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is started at port ${port}`))
+    }catch (error){
+        console.log(error);
+    }
+}
+
+start();
+
